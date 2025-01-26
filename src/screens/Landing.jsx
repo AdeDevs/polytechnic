6dc4e56fd8747618ce33a49cc661cc73c29ec34f
AdeDevs <adeyemiakinyemi01@gmail.com>
2025-01-26 02:49:39 +0100

    updt

--- src/screens/Landing.jsx
+++ src/screens/Landing.jsx
@@ -2,6 +2,7 @@ import Admission from "../assets/talent.png"
 import Screening from "../assets/connection.png"
 import Siwes from "../assets/technology.png"
 import Portal from "../assets/laptop.png"
+import About from "../assets/about.jpg"
 function HomePage() {
     return (
         <div className="home">
@@ -12,6 +13,7 @@ function HomePage() {
                 </p>
                 <button>join us today</button>
             </header>
+            <main>
             <div className="featured-links">
                 <h1>Featured Links</h1>
                 <ul className="links">
@@ -33,6 +35,51 @@ function HomePage() {
                     </li>
                 </ul>
             </div>
+            <div className="about-us">
+                <section className="about-image">
+                </section>
+                <section className="about-info">
+                    <h1>About Us</h1>
+                    <p>
+                        Best Solution Polytechnic, Akure is a fully licensed and accredited private polytechnic. Established in the year 2014, our aim is to achieve academic and technological excellence by revamping the educational system of Nigeria and proffering substantial solution to the technological needs of the nation.
+                    </p>
+                </section>
+                <section>
+                <div className="stats">
+                    <div className="stats-box">
+                        <div className="bar-info">
+                            <h1>Practical knowledge</h1>
+                            <p>95%</p>
+                        </div>
+                        <div className="bar">
+                            <div className="width"></div>
+                        </div>
+                    </div>
+                    <div className="stats-box">
+                        <div className="bar-info">
+                            <h1>Pass percentage</h1>
+                            <p>90%</p>
+                        </div>
+                        <div className="bar">
+                            <div className="js"></div>
+                        </div>
+                    </div>
+                    <div className="stats-box">
+                        <div className="bar-info">
+                            <h1>happy students</h1>
+                            <p>98%</p>
+                        </div>
+                        <div className="bar">
+                            <div className="ts"></div>
+                        </div>
+                    </div>
+                </div>
+                </section>
+            </div>
+            <div className="our-schools">
+                <h1>our schools</h1>
+            </div>
+            </main>
         </div>
     )
 }
