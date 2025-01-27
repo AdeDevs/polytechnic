da5800a97e43ec4e75f8addbf213ec942b9ce15b
AdeDevs <adeyemiakinyemi01@gmail.com>
2025-01-27 19:29:48 +0100

    updt

--- src/screens/Nav.jsx
+++ src/screens/Nav.jsx
@@ -1,5 +1,11 @@
 import Logo from "../assets/logo.png";
+import { useState } from "react";
 function Navigation() {
+  const [menuActive, isMenuActive] = useState(false)
+  
+  const toggleMenu = () => {
+      isMenuActive(!menuActive)
+  }
   return (
     <div className="nav-page">
       <div className="nav-bar">
@@ -13,6 +19,23 @@ function Navigation() {
           <li><a href="#">admission</a></li>
           <li><a href="#">news and events</a></li>
           <li><a href="#">international office</a></li>
+        </ul>
+        <span className="toggle-menu" onClick={toggleMenu}>
+                  <ion-icon name="menu-outline" />
+                </span>
+
+                
+        <div className={`overlay ${menuActive ? 'show' : ""}`} ></div>
+        <ul className={`hamburger-menu ${menuActive ? 'show' : ""}`}>
+                    <span className="toggle-menu">
+                      <ion-icon name="close-outline" onClick={toggleMenu} />
+                    </span>
+          <a href="#">home</a>
+          <a href="#">about us</a>
+          <a href="#">academics</a>
+          <a href="#">admission</a>
+          <a href="#">news and events</a>
+          <a href="#">international office</a>
         </ul>
       </div>
     </div>
